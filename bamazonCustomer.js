bb1b019f09e67eb86dfabdcd4ecda9421033d18f
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-05-13 18:25:06 -0400

    read complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,11 +5,15 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "root",
-    databse: "bamazon"
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
     connection.end();
 });
